bb52ae02ec5445d096864ca4e1541b0cf607c096
Rafael <rafa_pc_sjc@hotmail.com>
2023-03-28 11:47:21 -0300

    react projects 7

---
+++ class7/tst1/front/src/App.tsx
@@ -0,0 +1,16 @@
+import A from "./components/A";
+import { Provider } from "./contexts";
+
+
+function App() {
+
+   return (
+
+        <Provider>
+            <A />
+        </Provider>
+
+  );
+}
+
+export default App;
---
+++ class7/tst1/front/src/components/C.tsx
@@ -0,0 +1,9 @@
+import useExemplo from "../hooks/useExemplo";
+
+
+function C(){
+    const {nome,idade} = useExemplo();
+    return <div><div>Nome: {nome} Idade:{idade} </div></div>;
+  }
+
+export default C;
